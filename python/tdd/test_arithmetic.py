98482777e496ba588ff46170aabcd47457ad2c8f
Scott Trumpy <trumpysm@dukes.jmu.edu>
2018-03-02 10:35:42 -0500

    basic python code

---
+++ python/tdd/test_arithmetic.py
@@ -0,0 +1,15 @@
+# test_arithmetic.py
+
+# import the code to be tested
+from sum import sum
+
+# write a smoke test
+def test_smoke():
+	assert True == True
+
+# test that sum a,b == a + b
+def test_sum():
+  assert sum(3,4) == 7
+  assert sum(5.5,12.1) == 17.6
+  assert sum(-4,21) == 17
+  assert sum(1234,5678) == 6912
