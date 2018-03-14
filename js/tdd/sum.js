ba00c08e745cf3efa67c7f341826b23e54f25eb2
Scott Trumpy <trumpysm@dukes.jmu.edu>
2018-03-13 20:34:43 -0400

    initial commit

---
+++ js/tdd/sum.js
@@ -0,0 +1,19 @@
+/**
+ * A function to calculate the sum of two numbers
+ * @param  {[number]} a the first number to be added
+ * @param  {[number]} b the second number to be added
+ * @return {[number]} the sum of a and b
+ */
+
+let sum = (a,b) => {
+	return a + b;
+};
+
+module.exports = sum;
+
+
+
+//function sum(a, b) {
+//  return a + b;
+//}
+
